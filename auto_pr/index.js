077e4f94c049126200cc5f5569a0755937d5f877
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-10 22:09:17 -0700

    no top level await in node 12 :(

--- auto_pr/index.js
+++ auto_pr/index.js
@@ -4,12 +4,14 @@ const octokit = new Octokit({
   auth: process.env.RUNEQL_DATA_PUSH,
 });
 
-let res = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
-  owner: "schmidlidev",
-  repo: "runeql-data",
-  head: "update",
-  base: "main",
-  title: "OSRSBOX Data Update",
-});
-
-console.log(res);
+octokit
+  .request("POST /repos/{owner}/{repo}/pulls", {
+    owner: "schmidlidev",
+    repo: "runeql-data",
+    head: "update",
+    base: "main",
+    title: "OSRSBOX Data Update",
+  })
+  .then((res) => {
+    console.log(res);
+  });
