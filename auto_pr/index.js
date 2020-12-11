98436e3e4f804c3b47721230f482b2303fc9a0e2
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-11 16:14:10 -0700

    replace raw node12 env with node15 docker env

--- auto_pr/index.js
+++ auto_pr/index.js
@@ -1,21 +1,15 @@
-const core = require("@actions/core");
-const { Octokit } = require("@octokit/core");
+import { Octokit } from "@octokit/core";
 
 const octokit = new Octokit({
   auth: process.env.RUNEQL_DATA_PUSH,
 });
 
-octokit
-  .request("POST /repos/{owner}/{repo}/pulls", {
-    owner: "schmidlidev",
-    repo: "runeql-data",
-    head: "update",
-    base: "main",
-    title: "OSRSBOX Data Update",
-  })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+let res = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
+  owner: "schmidlidev",
+  repo: "runeql-data",
+  head: "update",
+  base: "main",
+  title: "OSRSBOX Data Update",
+});
+
+console.log(res);
