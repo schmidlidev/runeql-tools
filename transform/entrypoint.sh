92c824e578e58a30abc47e72b6e8089b43f5812f
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-12 15:42:52 -0700

    use runeql-bot

--- transform/entrypoint.sh
+++ transform/entrypoint.sh
@@ -6,8 +6,8 @@ timestamp=`date +%Y-%m-%d_%H:%M:%S`
 
 cd /transform
 
-git config --global user.email "schmidlidev@gmail.com"
-git config --global user.name "schmidlidev"
+git config --global user.email "runeqlbot@gmail.com"
+git config --global user.name "runeql-bot"
 
 git clone --single-branch --branch update --depth 1 https://github.com/schmidlidev/runeql-data.git
 git clone --single-branch --branch master --depth 1 https://github.com/schmidlidev/osrsbox-db.git 
@@ -18,7 +18,7 @@ python /transform/transform_items.py --input /transform/osrsbox-db/docs/items-js
 
 echo "Committing data to runeql-data"
 cd /transform/runeql-data/
-git remote set-url origin https://schmidlidev:${RUNEQL_DATA_LOAD}@github.com/schmidlidev/runeql-data.git
+git remote set-url origin https://runeql-bot:${RUNEQL_BOT_KEY}@github.com/schmidlidev/runeql-data.git
 git add .
 git status
 git commit -m "Data Update ${timestamp}"
