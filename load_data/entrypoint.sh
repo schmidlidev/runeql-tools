0b0b136433356e397a143d56cb129b2af084bb69
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-24 17:21:51 -0700

    input from env

--- load_data/entrypoint.sh
+++ load_data/entrypoint.sh
@@ -2,4 +2,4 @@
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
-python /load_data/load_data.py --input /github/workspace --mongo_uri $MONGO_URI
+python /load_data/load_data.py --input $INPUT --mongo_uri $MONGO_URI
